3a91ac8c4cebd7982ac7548d6f380182f619bfa1
Пётр <dimok392@mail.ru>
2022-05-22 12:58:51 +0300

    example/example001

---
+++ example/example001/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива
+Console.Write("Введите число строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine());
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(1, 9);
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int save;
+for (int i = 0; i < mass.GetLength(1); i++)
+{
+    save = mass[0,i];
+    mass[0,i] = mass[mass.GetLength(0) - 1, i];
+    mass[mass.GetLength(0) - 1, i]=save;
+}
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
