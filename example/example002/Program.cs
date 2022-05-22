511740852bbaad8f2db5a66a8b2136f6e5e6a80e
Пётр <dimok392@mail.ru>
2022-05-22 14:00:25 +0300

    example/example002

---
+++ example/example002/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+Console.Write("Введите число строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int n = int.Parse(Console.ReadLine());
+if (m!=n)
+{
+    System.Console.WriteLine("Решить задачу невозможно");
+    return;
+}
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(1, 9);
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int j = 0; j < mass.GetLength(0); j++)
+{
+    for (int i = 0; i < mass.GetLength(1); i++)
+    {
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
